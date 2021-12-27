960d4487ee01af0c72a7fabf9fe1c60c7daa4bc1
DESKTOP-2U5NDQ1\jw.lee <jw.lee@twolinecode.com>
2021-12-27 12:45:39 +0900

    [front] 삭제 팝업 추가

--- front_end/src/components/BlogListDetail.vue
+++ front_end/src/components/BlogListDetail.vue
@@ -5,11 +5,17 @@
       <div>{{ boardInfo.created }}</div>
       <div class="edit_container">
         <div class="edit_item">수정</div>
-        <div class="edit_item" @click="onDelete">삭제</div>
+        <div class="edit_item" @click="onDelete()">삭제</div>
       </div>
     </div>
     <div class="line"></div>
     <div id="viewer" />
+    <Popup
+      :showPoup.sync="deletePopup.showPoup"
+      :title="deletePopup.title"
+      :bodyContent="deletePopup.content"
+      @onButtonEvnet="onPopupButtonEvnet"
+    />
   </div>
 </template>
 
@@ -17,12 +23,22 @@
 import axios from "axios";
 import { CommonMixin } from "@/mixins/CommonMixin.js";
 import Viewer from "@toast-ui/editor/dist/toastui-editor-viewer";
+import Popup from "@/components/Popup";
 export default {
+  components: {
+    Popup,
+  },
   mixins: [CommonMixin],
   data() {
     return {
       boardInfo: {},
       toastUiViewer: {},
+
+      deletePopup: {
+        showPoup: false,
+        title: "",
+        content: "",
+      },
     };
   },
   created() {
@@ -51,18 +67,25 @@ export default {
       });
     },
     onDelete() {
-      axios
-        .delete(this.HOST_ADD + "board/" + this.$route.params.id)
-        .then((response) => {
-          console.log("onDelete response", response);
-          if (response.status == 200) {
-            this.showToastSuccess(`"${this.boardInfo.title}" 삭제 완료`);
-            this.$router.back();
-          }
-        })
-        .catch((error) => {
-          this.showToastError(error);
-        });
+      this.deletePopup.title = `글 삭제`;
+      this.deletePopup.content = `"${this.boardInfo.title}"을 삭제 하시겠습니까?`;
+      this.deletePopup.showPoup = true;
+    },
+    onPopupButtonEvnet(confirm) {
+      if (confirm) {
+        axios
+          .delete(this.HOST_ADD + "board/" + this.$route.params.id)
+          .then((response) => {
+            console.log("onDelete response", response);
+            if (response.status == 200) {
+              this.showToastSuccess(`"${this.boardInfo.title}" 삭제 완료`);
+              this.$router.back();
+            }
+          })
+          .catch((error) => {
+            this.showToastError(error);
+          });
+      }
     },
   },
 };
---
+++ front_end/src/components/Popup.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="popup-container" v-if="showPoup">
+    <div class="popup-content">
+      <div class="popup-header">
+        <div class="title">{{ title }}</div>
+        <div class="close-icon" @click="closePopup()">
+          <div class="material-icons type_size">close</div>
+        </div>
+      </div>
+      <div class="popup-body">
+        <div class="body-content">{{ bodyContent }}</div>
+        <div class="button-container">
+          <div class="button" @click="onConfirmButton()">확인</div>
+          <div class="button" @click="onCancelButton()">취소</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  watch: {
+    showPoup() {
+      console.log("dffsfs");
+    },
+  },
+  props: {
+    showPoup: Boolean,
+    title: String,
+    bodyContent: String,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    closePopup() {
+      this.$emit("update:showPoup", false);
+    },
+    onConfirmButton() {
+      this.closePopup();
+      this.$emit("onButtonEvnet", true);
+    },
+    onCancelButton() {
+      this.closePopup();
+      this.$emit("onButtonEvnet", false);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.popup-container {
+  z-index: 1000;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+
+  .popup-content {
+    border-radius: 4px;
+    background-color: white;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    margin-top: -112px;
+    margin-left: -225px;
+    width: 450px;
+    box-shadow: 0 2px 4px 0 rgba(black, 0.5px);
+
+    .popup-header {
+      background-color: rgb(26, 188, 156);
+      border-radius: 4px 4px 0 0;
+      border-bottom: 2px solid rgb(70, 70, 70);
+      display: flex;
+      justify-content: space-between;
+      flex-direction: row;
+      .title {
+        font-size: 26px;
+        font-weight: bold;
+        color: rgb(231, 231, 231);
+        padding: 17px;
+        margin-bottom: 6px;
+      }
+      .close-icon {
+        width: 30px;
+        margin-top: 9px;
+        margin-right: 5px;
+        cursor: pointer;
+        color: rgb(49, 49, 49);
+        &:hover {
+          color: rgb(0, 0, 0);
+        }
+      }
+    }
+
+    .popup-body {
+      display: flex;
+      flex-direction: column;
+      .body-content {
+        padding: 20px;
+        font-size: 20px;
+      }
+      .button-container {
+        display: flex;
+        justify-content: center;
+        .button {
+          display: inline-block;
+          padding: 10px;
+          margin: 10px;
+          color: white;
+          background-color: rgb(26, 188, 156);
+          border-radius: 4px;
+          cursor: pointer;
+          box-shadow: 2px 2px 5px #999;
+          &:hover {
+            background-color: rgb(5, 158, 128);
+          }
+        }
+      }
+    }
+  }
+}
+</style>
