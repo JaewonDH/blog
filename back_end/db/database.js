c386515669b351f1da1b8082606475c87cc9a26e
DESKTOP-2U5NDQ1\jw.lee <jw.lee@twolinecode.com>
2021-12-28 10:37:13 +0900

    [back] 글 업데이트 기능 추가

--- back_end/db/database.js
+++ back_end/db/database.js
@@ -1,92 +1,96 @@
-let mySQL = require('mysql');
+let mySQL = require("mysql");
 class Database {
-    queryStr = {
-        INSERT_BOARD_QUERY: `INSERT INTO board (title,tag,content,created) VALUE(?,?,?,NOW())`,
-        ALL_SELECT_BOARD_QUERY: `SELECT board.id,title,tag.name,content,created FROM board INNER JOIN tag ON board.tag=tag.id`,
-        SEARCH_TAG_NAME_QUERY: 'SELECT * FROM tag where name=?',
-        INSERT_TAG_QUERY: `INSERT INTO tag (name) VALUE(?)`,
-        ALL_SELECT_TAG_QUERY: 'SELECT * FROM tag',
-        WHERE_TAG_NAME_QUERY: 'WHERE tag.id=?',
-        ORDER_BY_QUERY:'ORDER BY created DESC',
-        SELECT_TAG_QUERY: `SELECT tag.id, tag.name, count(tag.id) as 'count' FROM board INNER JOIN tag ON board.tag=tag.id group by tag.id`,
-        BOARD_ALL_COUNT:'SELECT count(*) AS count FROM board ',
-        SELECT_BOARD_ITEM:'WHERE board.id=?',
-        DELETE_BOARD_ITEM:'DELETE FROM board WHERE id=?'
-        // SELECT board.id,title,tag.name,content,created FROM board INNER JOIN tag ON board.tag=tag.id WHERE tag.id=?
-    }
+  queryStr = {
+    INSERT_BOARD_QUERY: `INSERT INTO board (title,tag,content,created) VALUE(?,?,?,NOW())`,
+    ALL_SELECT_BOARD_QUERY: `SELECT board.id,title,tag.name,content,created FROM board INNER JOIN tag ON board.tag=tag.id`,
+    SEARCH_TAG_NAME_QUERY: "SELECT * FROM tag where name=?",
+    INSERT_TAG_QUERY: `INSERT INTO tag (name) VALUE(?)`,
+    ALL_SELECT_TAG_QUERY: "SELECT * FROM tag",
+    WHERE_TAG_NAME_QUERY: "WHERE tag.id=?",
+    ORDER_BY_QUERY: "ORDER BY created DESC",
+    SELECT_TAG_QUERY: `SELECT tag.id, tag.name, count(tag.id) as 'count' FROM board INNER JOIN tag ON board.tag=tag.id group by tag.id`,
+    BOARD_ALL_COUNT: "SELECT count(*) AS count FROM board ",
+    SELECT_BOARD_ITEM: "WHERE board.id=?",
+    DELETE_BOARD_ITEM: "DELETE FROM board WHERE id=?",
+    UPDATE_BOARD_ITEM: "UPDATE board SET title=?,tag=?,content=? WHERE id=?",
+    // SELECT board.id,title,tag.name,content,created FROM board INNER JOIN tag ON board.tag=tag.id WHERE tag.id=?
+  };
 
-    databaseInfo = {
-        host: 'localhost',
-        user: 'root',
-        password: '1234',
-        database: 'blog',
-        timezone: 'KST' //NOW() 타임존 설정
-    }
+  databaseInfo = {
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "blog",
+    timezone: "KST", //NOW() 타임존 설정
+  };
 
-    constructor() {
-        this.db = mySQL.createConnection(this.databaseInfo);
-        this.db.connect();
-    }
+  constructor() {
+    this.db = mySQL.createConnection(this.databaseInfo);
+    this.db.connect();
+  }
 
-    insertBoard(values) {
-        return this.query(this.queryStr.INSERT_BOARD_QUERY, values);
-    }
+  insertBoard(values) {
+    return this.query(this.queryStr.INSERT_BOARD_QUERY, values);
+  }
 
-    getBoardList(id,callback) {
-        let query= (id!=0) ? 
-            `${this.queryStr.ALL_SELECT_BOARD_QUERY} ${this.queryStr.WHERE_TAG_NAME_QUERY} ${this.queryStr.ORDER_BY_QUERY}` :
-            `${this.queryStr.ALL_SELECT_BOARD_QUERY} ${this.queryStr.ORDER_BY_QUERY}`;       
-        console.log('query',query)
-        return this.db.query(query,id,callback);
-    }
+  updateBoard(values, id) {
+    return this.query(this.queryStr.UPDATE_BOARD_ITEM, values, id);
+  }
 
-    deleteBoardItem(id){
-        return this.query(this.queryStr.DELETE_BOARD_ITEM,id);
-    }
+  getBoardList(id, callback) {
+    let query =
+      id != 0
+        ? `${this.queryStr.ALL_SELECT_BOARD_QUERY} ${this.queryStr.WHERE_TAG_NAME_QUERY} ${this.queryStr.ORDER_BY_QUERY}`
+        : `${this.queryStr.ALL_SELECT_BOARD_QUERY} ${this.queryStr.ORDER_BY_QUERY}`;
+    console.log("query", query);
+    return this.db.query(query, id, callback);
+  }
 
-    getBoardItem(id){
-        let query= `${this.queryStr.ALL_SELECT_BOARD_QUERY} ${this.queryStr.SELECT_BOARD_ITEM}`
-        return this.query(query,id);
-    }
+  deleteBoardItem(id) {
+    return this.query(this.queryStr.DELETE_BOARD_ITEM, id);
+  }
 
-    getBoardCount(){
-        return this.query(this.queryStr.BOARD_ALL_COUNT);
-    }
+  getBoardItem(id) {
+    let query = `${this.queryStr.ALL_SELECT_BOARD_QUERY} ${this.queryStr.SELECT_BOARD_ITEM}`;
+    return this.query(query, id);
+  }
 
-    isTagName(tagName) {
-        return this.query(this.queryStr.SEARCH_TAG_NAME_QUERY,tagName);
-    }
-    
-    addTagName(tagName){
-        return this.query(this.queryStr.INSERT_TAG_QUERY,tagName);
-    }
+  getBoardCount() {
+    return this.query(this.queryStr.BOARD_ALL_COUNT);
+  }
 
-    getTagList(){
-        return this.query(this.queryStr.SELECT_TAG_QUERY);
-    }
+  isTagName(tagName) {
+    return this.query(this.queryStr.SEARCH_TAG_NAME_QUERY, tagName);
+  }
 
-    query(queryStr, values) {
-        return new Promise( (resolve, reject) => {
-            if (values == null) {
-                this.db.query(queryStr, (error, results)=>{               
-                    if(error==null){
-                        resolve(results);
-                    }else{
-                        reject(error);
-                    }
-                });
-            } else {
-                this.db.query(queryStr, values, (error, results)=>{            
-                    if(error==null){
-                        console.log('1')
-                        resolve(results);
-                    }else{
-                        console.log('2')
-                        reject(error);
-                    }
-                });
-            }
+  addTagName(tagName) {
+    return this.query(this.queryStr.INSERT_TAG_QUERY, tagName);
+  }
+
+  getTagList() {
+    return this.query(this.queryStr.SELECT_TAG_QUERY);
+  }
+
+  databaseResultPromise(error, results, resolve, reject) {
+    if (error == null) {
+      resolve(results);
+    } else {
+      reject(error);
+    }
+  }
+
+  query(queryStr, values = null) {
+    return new Promise((resolve, reject) => {
+      if (values == null) {
+        this.db.query(queryStr, (error, results) => {
+          this.databaseResultPromise(error, results, resolve, reject);
         });
-    }   
+      } else {
+        this.db.query(queryStr, values, (error, results) => {
+          this.databaseResultPromise(error, results, resolve, reject);
+        });
+      }
+    });
+  }
 }
-module.exports =new Database(mySQL);
+module.exports = new Database(mySQL);
--- back_end/webServer.js
+++ back_end/webServer.js
@@ -1,100 +1,150 @@
-const express = require('express');
-const database = require('./db/database');
-const cors = require('cors'); // Access-Control-Allow-Origin  해결 모듈
+const express = require("express");
+const database = require("./db/database");
+const cors = require("cors"); // Access-Control-Allow-Origin  해결 모듈
 
-const multer = require('multer'); // // form-data를 받을 때 필요 
-const { response } = require('express');
+const multer = require("multer"); // // form-data를 받을 때 필요
+const { response } = require("express");
+const { addTagName } = require("./db/database");
 const formData = multer({
-    dest: 'uploads/'
-}) // form-data를 받을 때 필요 
+  dest: "uploads/",
+}); // form-data를 받을 때 필요
 
 const app = express();
 let portNumber = 3333;
-app.use(express.static('public')); //특정 폴더의 접근을 허가 
-app.use(express.urlencoded({
-    extended: false
-})); //포스트로 일반 json object를 받을 때 필요 
-app.use(express.json()) //포스트로 일반 json object를 받을 때 필요 
+app.use(express.static("public")); //특정 폴더의 접근을 허가
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+); //포스트로 일반 json object를 받을 때 필요
+app.use(express.json()); //포스트로 일반 json object를 받을 때 필요
 app.use(cors());
 
 app.listen(portNumber, () => {
-    console.log(`start ${portNumber} prot`);
+  console.log(`start ${portNumber} prot`);
 });
 
-// blog 리스트 목록 
-app.get('/boardList/:id', function (request, response) {
-    console.log('request.params.id',request.params.id)
-    database.getBoardList(Number(request.params.id),(error, results) => {
-        console.log('request.params.id error',error);
-        if (error == null) {
-            response.status(200).send(results);
-        }
-    });
-})
+// blog 리스트 목록
+app.get("/boardList/:id", function (request, response) {
+  console.log("request.params.id", request.params.id);
+  database.getBoardList(Number(request.params.id), (error, results) => {
+    console.log("request.params.id error", error);
+    if (error == null) {
+      response.status(200).send(results);
+    }
+  });
+});
 
 // blog 글 추가
-app.post('/write', formData.array(), function (request, response) { // front 쪽에서 post 전달 시 form data 형식으로 받을 경우        
-    let body = request.body;
-    console.log('body', body);
-    database.isTagName(body.tag).then(result => {
-        if (result.length == 0) {
-            return database.addTagName(body.tag);
-        } else {
-            return result[0].id;
-        }
-    }).then(result => {
-        let tagID
-        if (typeof result == 'object') {
-            tagID = result.insertId
-        } else {
-            tagID = result;
-        }
-        return database.insertBoard([body.title, tagID, body.content, ]);
-    }).then(result => {
-        console.log('result', result);
-        response.status(200).send('게시글 등록 완료');
+app.post("/write", formData.array(), function (request, response) {
+  // front 쪽에서 post 전달 시 form data 형식으로 받을 경우
+  let body = request.body;
+  console.log("body", body);
+  database
+    .isTagName(body.tag)
+    .then((result) => {
+      console.log("result", result);
+      return getTagName(result, body.tag);
     })
-    .catch(error => {
-        console.log('error', error);
-        response.status(400).send(error);
+    .then((result) => {
+      return database.insertBoard([body.title, getTagID(result), body.content]);
+    })
+    .then((result) => {
+      console.log("result", result);
+      response.status(200).send("게시글 등록 완료");
+    })
+    .catch((error) => {
+      console.log("error", error);
+      response.status(400).send(error);
     });
-})
+});
 
-// board 항목의 상세 정보 가져 오기 
-app.get('/boardInfo/:id', (request, response) => {
-    console.log('getBoardItem request.params.id', request.params.id)
-    database.getBoardItem(request.params.id).then(results => {
-        let object = results.length > 0 ? results[0] : [];
-        response.status(200).send(object);
-    }).catch(error => {
-        response.status(400).send(error);
+// board 항목의 상세 정보 가져 오기
+app.get("/boardInfo/:id", (request, response) => {
+  console.log("getBoardItem request.params.id", request.params.id);
+  database
+    .getBoardItem(request.params.id)
+    .then((results) => {
+      let object = results.length > 0 ? results[0] : [];
+      response.status(200).send(object);
     })
+    .catch((error) => {
+      response.status(400).send(error);
+    });
 });
 
 // blog tag 목록 가져오기
-app.get('/tagList', function (request, response) {
-    let result={}
-    database.getBoardCount().then(resultDB=>{
-        result.totalCount=resultDB[0].count;
-        console.log('result',result);
-        return database.getTagList();
-    }).then(resultDb=>{
-        result.tagList=resultDb;
-        console.log('result',resultDb);
-        response.status(200).send(result);
-    }).catch(error=>{
-        console.log('error',error);
-        response.status(400).send(error);
-    });
-})
-
-app.delete('/board/:id',(request,response)=>{
-    console.log('deleteBoardItem request.params.id', request.params.id)
-    database.deleteBoardItem(request.params.id).then(result=>{       
-        response.status(200).send('삭제완료');
-    }).catch(error=>{
-        console.log('deleteBoardItem error',error);
-        response.status(400).send(error);
+app.get("/tagList", function (request, response) {
+  let result = {};
+  database
+    .getBoardCount()
+    .then((resultDB) => {
+      result.totalCount = resultDB[0].count;
+      console.log("result", result);
+      return database.getTagList();
+    })
+    .then((resultDb) => {
+      result.tagList = resultDb;
+      console.log("result", resultDb);
+      response.status(200).send(result);
+    })
+    .catch((error) => {
+      console.log("error", error);
+      response.status(400).send(error);
     });
 });
 
+// 삭제 기능
+app.delete("/board/:id", (request, response) => {
+  console.log("deleteBoardItem request.params.id", request.params.id);
+  database
+    .deleteBoardItem(request.params.id)
+    .then((result) => {
+      response.status(200).send("삭제완료");
+    })
+    .catch((error) => {
+      console.log("deleteBoardItem error", error);
+      response.status(400).send(error);
+    });
+});
+
+// 수정 기능
+app.put("/boardInfo/:id", formData.array(), (request, response) => {
+  console.log("update body", request.body);
+  let body = request.body;
+  database
+    .isTagName(body.tag)
+    .then((result) => {
+      return getTagName(result, body.tag);
+    })
+    .then((result) => {
+      return database.updateBoard([
+        body.title,
+        getTagID(result),
+        body.content,
+        request.params.id,
+      ]);
+    })
+    .then((result) => {
+      response.status(200).send("수정 완료");
+    })
+    .catch((error) => {
+      response.status(400).send(error);
+    });
+});
+
+function getTagName(result, tag) {
+  if (result.length == 0) {
+    return database.addTagName(tag);
+  } else {
+    return result[0].id;
+  }
+}
+
+function getTagID(result) {
+  if (typeof result == "object") {
+    return result.insertId;
+  } else {
+    return result;
+  }
+}
